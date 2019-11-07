54a85aecf364c55b29944e777435630043e01034
Aastha02-7 <53691699+Aastha02-7@users.noreply.github.com>
2019-11-07 21:02:19 +0700

    Add files via upload

---
+++ display_array.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int x,a[5],i;
+	printf("Enter five elements:\n");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("Entered numbers are:\n");
+	for(i=0;i<5;i++)
+	{
+		printf("%d\n",&a[i]);
+	}
+	return 0;n
+}
